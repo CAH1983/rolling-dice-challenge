5bbf94fe71bdd119a442c125ea6afd42a8c85183
Caroline Ho <carolineho1983@gmail.com>
2022-01-03 19:28:15 +0000

    added components and roll function

--- src/Components/Dice.js
+++ src/Components/Dice.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import "../Dice.css";
 
 class Dice extends Component {
   render() {
     return (
       <div className="Dice">
-        <i className='"fas fa-dice-one'></i>
+        <div className="Dice-icon-wrapper">
+          <i className="fas fa-dice-one"></i>
+        </div>
       </div>
     );
   }
--- src/Components/RollDice.js
+++ src/Components/RollDice.js
@@ -2,10 +2,23 @@ import React, { Component } from "react";
 import Dice from "./Dice";
 
 class RollDice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currNum: "one",
+    };
+  }
+
+  rollDices() {
+    const numbersArr = ["one", "two", "three", "four", "five", "six"];
+    const randomNum = numbersArr[Math.floor(Math.random() * numbersArr.length)];
+    this.setState({ currNum: randomNum });
+  }
   render() {
     return (
       <div className="RollDice">
-        <Dice />
+        <Dice number={this.state.currNum} />
+        <Dice number={this.state.currNum} />
         <button> Roll it </button>
       </div>
     );
