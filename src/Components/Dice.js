9638757cb2b8de82e10f59b7e153dde1d383c135
Caroline Ho <carolineho1983@gmail.com>
2022-01-08 21:49:32 +0000

    finished the challenge

--- src/Components/Dice.js
+++ src/Components/Dice.js
@@ -4,7 +4,7 @@ import "../Dice.css";
 class Dice extends Component {
   render() {
     return (
-      <div className="Dice">
+      <div className={`Dice ${this.props.extraclass}`}>
         <div className="Dice-icon-wrapper">
           <i className={`fas fa-dice-${this.props.number}`}></i>
         </div>
--- src/Components/RollDice.js
+++ src/Components/RollDice.js
@@ -8,6 +8,8 @@ class RollDice extends Component {
     this.state = {
       currNum: "one",
       currNum2: "two",
+      isRolling: false,
+      extraclass: null,
     };
 
     this.rollDices = this.rollDices.bind(this);
@@ -15,21 +17,43 @@ class RollDice extends Component {
 
   rollDices() {
     const numbersArr = ["one", "two", "three", "four", "five", "six"];
-    const randomNum = numbersArr[Math.floor(Math.random() * numbersArr.length)];
-    const randomNum2 =
-      numbersArr[Math.floor(Math.random() * numbersArr.length)];
+    let randomNum;
+    let randomNum2;
 
-    this.setState({ currNum: randomNum, currNum2: randomNum2 });
+    this.setState({
+      isRolling: true,
+      extraclass: "shaking",
+    });
+
+    setTimeout(() => {
+      randomNum = numbersArr[Math.floor(Math.random() * numbersArr.length)];
+      randomNum2 = numbersArr[Math.floor(Math.random() * numbersArr.length)];
+      this.setState({
+        currNum: randomNum,
+        currNum2: randomNum2,
+        isRolling: false,
+        extraclass: null,
+      });
+    }, 1000);
   }
 
   render() {
     return (
       <div className="RollDice">
         <div className="flex-line">
-          <Dice number={this.state.currNum} />
-          <Dice number={this.state.currNum2} />
+          <Dice
+            number={this.state.currNum}
+            extraclass={this.state.extraclass}
+          />
+          <Dice
+            number={this.state.currNum2}
+            extraclass={this.state.extraclass}
+          />
         </div>
-        <button onClick={this.rollDices}> Roll it </button>
+        <button disabled={this.state.isRolling} onClick={this.rollDices}>
+          {" "}
+          Roll it{" "}
+        </button>
       </div>
     );
   }
